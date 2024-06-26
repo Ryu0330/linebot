2fd894ad6d9e1df62b8efcc810963534b5923782
Ryu0330 <aoiryu33@gmail.com>
2024-06-27 04:40:25 +0900

    add just reply message

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-from fastapi import FastAPI,APIRouter
-from linebot import WebhookHandler
+from fastapi import FastAPI,APIRouter,Header,Request
 from linebot.v3.messaging import Configuration
+from linebot.exceptions import InvalidSignatureErro
+from linebot.models import (
+    MessageEvent, TextMessage, FollowEvent, UnfollowEvent,
+    TextSendMessage, ImageMessage, AudioMessage
+)
+from linebot import LineBotApi, WebhookHandler
+
+config = Configuration(access_token='LINE_CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('LINE_CHANNEL_SECRET')
 
 app = FastAPI()
 
-config = Configuration(access_token='LINE_CHANNEL_SECRET')
-handler = WebhookHandler('LINE_CHANNEL_ACCESS_TOKEN')
-router = APIRouter(prefix="", tags=["line_bot"])
+@app.post("/callback")
+async def callback(
+    request: Request,
+    x_line_signature=Header(None)
+):
+    body = await request.body()
+    body = body.decode
+    
+
+@handler.add(MessageEvent)
+def handle_message(event):
+    if event.type != "text":
+        return
+    responsemessage = event.text
+
+
