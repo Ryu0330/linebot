a3efaef37201ec79daa16ad115d3440d726c6ff9
Ryu0330 <aoiryu33@gmail.com>
2024-06-30 15:14:24 +0900

    add replying same text

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI,APIRouter,Header,Request
+from fastapi import FastAPI,HTTPException,Header,Request
 from linebot.v3.messaging import Configuration
-from linebot.exceptions import InvalidSignatureErro
+from linebot.exceptions import InvalidSignatureError
 from linebot.models import (
     MessageEvent, TextMessage, FollowEvent, UnfollowEvent,
     TextSendMessage, ImageMessage, AudioMessage
@@ -19,12 +19,20 @@ async def callback(
 ):
     body = await request.body()
     body = body.decode
-    
+    try:
+        handler.handle(body,x_line_signature)
+    except InvalidSignatureError:
+        raise HTTPException(status_code=404, detail='InvalidSignatureError')
+    return 'np'
 
 @handler.add(MessageEvent)
 def handle_message(event):
     if event.type != "text":
         return
-    responsemessage = event.text
+    
+    LineBotApi.reply_message(
+        reply_token=event.reply_token,
+        messages=TextMessage(text=event.message.text)
+    )
 
 
